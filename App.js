66dcd1a7c669041f7c8e5568fadf6cf9029e4e1d
Johan Pieterse <ja.pieterse3@gmail.com>
2021-02-14 15:03:53 +0200

    bubble spawns at random location

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableWithoutFeedback, Image, Dimensions} from 'react-native';
+import IMAGE_EXPO from './assets/triangle.png'
+
+const IMAGE_SIZE = 100;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [score, setScore] = useState(0);
+    const [position, setPosition] = useState(getRandXY);
+
+    return (
+        <View style={{flex: 1, backgroundColor: 'steelblue'}}>
+            <Text style={{fontSize: 72, textAlign: 'center'}}>{score}</Text>
+            <TouchableWithoutFeedback onPressIn={() => increaseScore()}>
+                <Image source={IMAGE_EXPO} style={{
+                    width: IMAGE_SIZE,
+                    height: IMAGE_SIZE,
+                    left: position.x,
+                    top: position.y,
+                    position: 'absolute'
+                }}/>
+            </TouchableWithoutFeedback>
+        </View>
+    );
+
+    function increaseScore() {
+        setScore(score + 1);
+        setPosition(getRandXY());
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+function getRandXY() {
+    return {
+        x: getRandInt(0, Dimensions.get('window').width - IMAGE_SIZE),
+        y: getRandInt(0, Dimensions.get('window').height - IMAGE_SIZE)
+    }
+}
+
+function getRandInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
